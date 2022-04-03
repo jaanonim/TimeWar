a399fe34a86194c46676ab6250df1c56164648f8
n2oneProgrammer <kubawojtasik70@gmail.com>
2022-04-03 15:28:14 +0200

    add todo and change names

--- client/src/scripts/MapCreator.js
+++ client/src/scripts/MapCreator.js
@@ -11,6 +11,7 @@ export default class MapCreator {
     }
 
     constructor() {
+        //TODO: change to get this from server
         this.map = [
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -24,15 +25,15 @@ export default class MapCreator {
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         ];
         this.mapObjects = [];
-        this.width = 50;
-        this.height = 50;
+        this.mapWidth = 50;
+        this.mapHeight = 50;
     }
 
     createMap(scene) {
         let widthLength = this.map.length;
         let heightLength = this.map[0].length;
-        let tileWidth = this.width / widthLength;
-        let tileHeight = this.height / heightLength;
+        let tileWidth = this.mapWidth / widthLength;
+        let tileHeight = this.mapHeight / heightLength;
         this.mapObjects = [];
         let landFactory = new MapLandFactor();
         for (let x = 0; x < widthLength; x++) {
