faad1279bf4991e376b145f0e85c914788a42536
Jaanonim <mat8mro@gmail.com>
2022-04-03 17:56:32 +0200

    Main Page

--- client/src/pages/mainPage/MainPage.jsx
+++ client/src/pages/mainPage/MainPage.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
+import styles from "../../styles/MainPage.module.css";
 
 function MainPage() {
-  return (
-    <div style={{margin: "1rem"}}>
-        <h1>Main Page</h1>
-        <Link to="/game">GAME</Link>
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			<nav>
+				<h1 className={styles.logo}>TimeWar</h1>
+				<ul className={styles.navList}>
+					<li className={styles.navItem}>
+						<Link className={styles.navLink} to="/game">
+							Game
+						</Link>
+					</li>
+					<li className={styles.navItem}>
+						<Link className={styles.navLink} to="/settings">
+							Settings
+						</Link>
+					</li>
+					<li className={styles.navItem}>
+						<Link className={styles.navLink} to="/about">
+							About
+						</Link>
+					</li>
+				</ul>
+				<footer>jaanonim &amp; n2one &copy; 2021</footer>
+			</nav>
+			<aside>
+				<h2>TODO: Some 3d animation</h2>
+			</aside>
+		</div>
+	);
 }
 
-export default MainPage
+export default MainPage;
