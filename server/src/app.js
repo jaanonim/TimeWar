7364348d6e370051876e9e600b8096030f548c62
Jaanonim <mat8mro@gmail.com>
2022-04-05 21:45:39 +0200

    fix react routing server site

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const indexRouter = require('./routes');
 
@@ -8,5 +9,9 @@ app.use(express.json());
 app.use(express.static('../client/dist'));
 
 app.use('/api', indexRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
 
 module.exports = app;
