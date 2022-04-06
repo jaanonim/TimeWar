56886d44f8755ef0def90f59d2f5df2e326aefba
Jaanonim <mat8mro@gmail.com>
2022-04-06 20:50:25 +0200

    numbers in left panel

--- client/src/components/LeftPanel/LeftPanel.jsx
+++ client/src/components/LeftPanel/LeftPanel.jsx
@@ -1,26 +1,37 @@
+import { useState } from "react";
+import { FigureTypes } from "../../scripts/enums/FigureTypes";
+import FigureManager from "../../scripts/FigureManager";
+import GameManager from "../../scripts/GameManager";
 import FiguresPlaceSection from "../FiguresPlaceSection";
 import styles from "./LeftPanel.module.css";
-import {useState} from "react";
-import FigureManager from "../../scripts/FigureManager";
-import {FigureTypes} from "../../scripts/enums/FigureTypes";
-import GameManager from "../../scripts/GameManager";
 
 function LeftPanel() {
-    const [selectedFigureId, setSelectedFigureId] = useState(null);
-    const [selectedFigureType, setSelectedFigureType] = useState(null);
-    let landArmy = FigureManager.landArmy;
-    GameManager.instance.onSelectFigureInUI(selectedFigureId, selectedFigureType);
-    return (
-        <div className={`${styles.ui} ${styles.placementPanel}`}>
-            <FiguresPlaceSection name="Land Army"
-                                 figures={landArmy}
-                                 select={selectedFigureType === FigureTypes.ARMY ? selectedFigureId : -1}
-                                 selecting={(newId) => {
-                                     setSelectedFigureId(newId);
-                                     setSelectedFigureType(FigureTypes.ARMY);
-                                 }}/>
-        </div>
-    );
+	const [selectedFigureId, setSelectedFigureId] = useState(null);
+	const [selectedFigureType, setSelectedFigureType] = useState(null);
+	let landArmy = FigureManager.landArmy;
+	GameManager.instance.onSelectFigureInUI(
+		selectedFigureId,
+		selectedFigureType
+	);
+	return (
+		<div className={`${styles.ui} ${styles.placementPanel}`}>
+			<FiguresPlaceSection
+				name="Land Army"
+				figures={landArmy}
+				select={
+					selectedFigureType === FigureTypes.ARMY
+						? selectedFigureId
+						: -1
+				}
+				selecting={(newId) => {
+					setSelectedFigureId(newId);
+					setSelectedFigureType(FigureTypes.ARMY);
+				}}
+				value={2}
+				max={3}
+			/>
+		</div>
+	);
 }
 
 export default LeftPanel;
