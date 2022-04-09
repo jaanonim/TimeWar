00d403325aedcad6aca25110ba149652853b6e9e
Jaanonim <mat8mro@gmail.com>
2022-04-09 14:39:09 +0200

    better land structure

--- client/src/scripts/classes/MapLandFactor.js
+++ client/src/scripts/classes/MapLandFactor.js
@@ -1,20 +1,33 @@
-import {
-    MapLandTypes
-} from "../enums/MapTileTypes";
+import { MapLandTypes } from "../enums/MapTileTypes";
 import Land from "./lands/Land";
-import Mountain from "./lands/Mountain";
 
 export default class MapLandFactor {
-    createTile(x, y, mapPositionX, mapPositionY, width, height, type) {
-        switch (type) {
-            case MapLandTypes.LAND:
-                return new Land(x, y, mapPositionX, mapPositionY, width, height);
-            case MapLandTypes.MOUNTAIN:
-                return new Mountain(x, y, mapPositionX, mapPositionY, width, height);
-            case MapLandTypes.NOTHING:
-                return null;
-            default:
-                console.error("Unknown type " + type);
-        }
-    }
+	createTile(x, y, mapPositionX, mapPositionY, width, height, type) {
+		switch (type) {
+			case MapLandTypes.LAND:
+				return new Land(
+					x,
+					y,
+					mapPositionX,
+					mapPositionY,
+					width,
+					height,
+					"land"
+				);
+			case MapLandTypes.MOUNTAIN:
+				return new Land(
+					x,
+					y,
+					mapPositionX,
+					mapPositionY,
+					width,
+					height,
+					"mountain"
+				);
+			case MapLandTypes.NOTHING:
+				return null;
+			default:
+				console.error("Unknown type " + type);
+		}
+	}
 }
--- client/src/scripts/classes/lands/Land.js
+++ client/src/scripts/classes/lands/Land.js
@@ -2,14 +2,18 @@ import ModelsManager from "../../ModelsManager";
 import MapLand from "../MapLand";
 
 export default class Land extends MapLand {
-    constructor(x, y, mapPositionX, mapPositionY, width, height) {
-        super(x, y, mapPositionX, mapPositionY, width, height);
-        this.position.y = -0.5;
-        this.model = ModelsManager.models["land"].children[0].clone();
-        this.model.scale.set(1, 1, 1);
-        this.model.rotateY(Math.PI / 2 * Math.round(Math.random() * 4));
-        this.model.material = this.model.material.clone();
-        this.add(this.model);
-        this.unHighLight();
-    }
+	constructor(x, y, mapPositionX, mapPositionY, width, height, modelName) {
+		super(x, y, mapPositionX, mapPositionY, width, height);
+		this.position.y = -0.5;
+		if (ModelsManager.models[modelName] === undefined) {
+			console.error("Unknown model", modelName);
+			return;
+		}
+		this.model = ModelsManager.models[modelName].children[0].clone();
+		this.model.scale.set(1, 1, 1);
+		this.model.rotateY((Math.PI / 2) * Math.round(Math.random() * 4));
+		this.model.material = this.model.material.clone();
+		this.add(this.model);
+		this.unHighLight();
+	}
 }
