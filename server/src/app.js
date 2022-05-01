851c473e5eb508a482ca73c3669992d99c87c2be
Jaanonim <mat8mro@gmail.com>
2022-05-01 13:24:06 +0200

    fix-cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 
 const indexRouter = require("./routes");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static("../client/dist"));
