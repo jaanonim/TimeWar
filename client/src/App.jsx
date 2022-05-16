a7f83711f6e3d16038feb7b4db4dfaf24394039f
Jaanonim <mat8mro@gmail.com>
2022-05-16 16:26:26 +0200

    test 4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,17 @@
 import { default as React } from "react";
 import { Route, Routes } from "react-router-dom";
 import Loading from "./components/Loading";
-/*
 import GamePage from "./pages/GamePage";
 import JoinPage from "./pages/JoinPage";
 import MainPage from "./pages/MainPage";
-*/
 
+/*
 const GamePage = React.lazy(() => import("./pages/GamePage"));
 const MainPage = React.lazy(() => import("./pages/MainPage"));
 const JoinPage = React.lazy(() => import("./pages/JoinPage"));
+*/
 
 function App() {
-  return <Loading />;
   return (
     <Routes>
       <Route
