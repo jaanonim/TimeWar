7cfbd59993b28af4538876c3589cc22a08addea5
Jaanonim <mat8mro@gmail.com>
2022-05-21 19:36:36 +0200

    fix modal color in game

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,10 +1,16 @@
 import styles from "./Modal.module.css";
 
-function Modal({ children, backgroundClick, show = true }) {
+function Modal({ children, backgroundClick, backgroundColor, show = true }) {
   if (show)
     return (
       <div className={styles.modal}>
-        <div className={styles.background} onClick={backgroundClick}></div>
+        <div
+          className={styles.background}
+          onClick={backgroundClick}
+          style={{
+            backgroundColor: backgroundColor || "rgba(0, 0, 0, 0.3)",
+          }}
+        ></div>
         <div className={styles.box}>{children}</div>
       </div>
     );
