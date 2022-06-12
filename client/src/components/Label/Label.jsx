dc4f939d5693d76700d0078320cc5d90357ba01f
Jaanonim <mat8mro@gmail.com>
2022-06-12 19:02:31 +0200

    fix labels

--- client/src/components/Label/Label.jsx
+++ client/src/components/Label/Label.jsx
@@ -2,32 +2,36 @@ import ProgressBar from "../ProgressBar";
 import styles from "./Label.module.css";
 
 function Label(props) {
-  switch (props.type) {
-    case "lab":
-      const color = props.data.atacked
-        ? ""
-        : props.team == "RED"
-        ? styles.red
-        : styles.blue;
-      return (
-        <div className={styles.container}>
-          {props.richVersion ? props.name : null}
-          <div className={styles.circle + " " + color}></div>
-        </div>
-      );
+    switch (props.type) {
+        case "lab":
+            const color = props.data.atacked
+                ? ""
+                : props.team == "RED"
+                ? styles.red
+                : styles.blue;
+            return (
+                <div className={styles.container}>
+                    {props.richVersion ? props.name : null}
+                    <div className={styles.circle + " " + color}></div>
+                </div>
+            );
 
-    default:
-      return (
-        <div className={styles.container}>
-          {props.richVersion ? props.name : null}
-          <ProgressBar
-            activeColor={props.team == "RED" ? "#ff0000" : "#0000ff"}
-            size={props.maxLives}
-            value={props.lives}
-            variant={"health"}
-          ></ProgressBar>
-        </div>
-      );
-  }
+        default:
+            return (
+                <div className={styles.container}>
+                    {props.richVersion ? props.name : null}
+                    <div className={styles.progress}>
+                        <ProgressBar
+                            activeColor={
+                                props.team == "RED" ? "#ff0000" : "#0000ff"
+                            }
+                            size={props.maxLives}
+                            value={props.lives}
+                            variant={"health"}
+                        ></ProgressBar>
+                    </div>
+                </div>
+            );
+    }
 }
 export default Label;
--- client/src/components/ProgressBar/ProgressBar.jsx
+++ client/src/components/ProgressBar/ProgressBar.jsx
@@ -1,22 +1,25 @@
 import styles from "./ProgressBar.module.css";
 
 function ProgressBar({ size, value, variant = "normal", activeColor }) {
-  size = parseInt(size);
-  value = parseInt(value);
-  return size < value || value < 0 ? null : (
-    <div
-      className={styles.container + " " + styles[variant]}
-      style={{
-        "--color": activeColor,
-      }}
-    >
-      {[...Array(value).keys()].map((i) => (
-        <div className={styles.block + " " + styles.active} key={i}></div>
-      ))}
-      {[...Array(size - value).keys()].map((i) => (
-        <div className={styles.block} key={i}></div>
-      ))}
-    </div>
-  );
+    size = parseInt(size);
+    value = parseInt(value);
+    return size < value || value < 0 ? null : (
+        <div
+            className={styles.container + " " + styles[variant]}
+            style={{
+                "--color": activeColor,
+            }}
+        >
+            {[...Array(value).keys()].map((i) => (
+                <div
+                    className={styles.block + " " + styles.active}
+                    key={i}
+                ></div>
+            ))}
+            {[...Array(size - value).keys()].map((i) => (
+                <div className={styles.block} key={i}></div>
+            ))}
+        </div>
+    );
 }
 export default ProgressBar;
